e6575cf24bb6f146bcf342193d2fd83d5d087348
Fauzan Rizky <frizky.erlang@gmail.com>
2024-12-22 13:06:06 +0700

    fix test

--- src/Domains/threads/entities/_test/AddedThread.test.js
+++ src/Domains/threads/entities/_test/AddedThread.test.js
@@ -4,31 +4,34 @@ describe('a AddedThread entities', () => {
   it('should throw error when payload did not contain needed property', () => {
     // Arrange
     const payload = {
+      id: 'user-123',
       title: 'abc',
     };
 
     // Action and Assert
-    expect(() => new AddedThread(payload)).toThrowError('ADD_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+    expect(() => new AddedThread(payload)).toThrowError('ADDED_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
   });
 
   it('should throw error when payload did not meet data type specification', () => {
     // Arrange
     const payload = {
-      title: 123,
-      body: true,
+      id: 123,
+      title: {},
+      owner: true,
     };
 
     // Action and Assert
     expect(() => new AddedThread(payload)).toThrowError(
-      'ADD_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION',
+      'ADDED_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION',
     );
   });
 
   it('should create addedThread object correctly', () => {
     // Arrange
     const payload = {
+      id: 'thread-123',
       title: 'sebuah thread',
-      body: 'sebuah body thread',
+      owner: 'user-123',
     };
 
     // Action
