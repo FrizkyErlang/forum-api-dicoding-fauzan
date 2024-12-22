640a1cfc4fc03f573a2fff5a13454470324888af
Fauzan Rizky <frizky.erlang@gmail.com>
2024-12-22 13:01:24 +0700

    create Thread domainm repos

---
+++ src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+
+class ThreadRepository {
+  async verifyThread(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async addThread(addThread) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getThread(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ThreadRepository;
