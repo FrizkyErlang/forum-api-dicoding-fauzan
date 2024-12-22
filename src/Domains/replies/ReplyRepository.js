33101ed33b49765528a0d880cd36ea90c86af810
Fauzan Rizky <frizky.erlang@gmail.com>
2024-12-22 15:25:54 +0700

    create Reply domain repo

---
+++ src/Domains/replies/ReplyRepository.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+
+class ReplyRepository {
+  async verifyReply(replyId) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyReplyOwner(replyId, owner) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async addReply(addReply) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getReply(replyId) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteReply(replyId) {
+    throw new Error('REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ReplyRepository;
