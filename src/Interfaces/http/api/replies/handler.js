2b01258efb128e669edcad71c0015dbb881e278d
Fauzan Rizky <frizky.erlang@gmail.com>
2024-12-23 20:37:23 +0700

    add reply route ad test

---
+++ src/Interfaces/http/api/replies/handler.js
@@ -0,0 +1,39 @@
+const autoBind = require('auto-bind');
+const AddReplyUseCase = require('../../../../Applications/use_case/AddReplyUseCase');
+const DeleteReplyUseCase = require('../../../../Applications/use_case/DeleteReplyUseCase');
+
+class RepliesHandler {
+  constructor(container) {
+    this._container = container;
+
+    autoBind(this);
+  }
+
+  async postReplyHandler(request, h) {
+    const { id: owner } = request.auth.credentials;
+    const { threadId, commentId } = request.params;
+    const addReplyUseCase = this._container.getInstance(AddReplyUseCase.name);
+    const addedReply = await addReplyUseCase.execute(request.payload, commentId, threadId, owner);
+
+    const response = h.response({
+      status: 'success',
+      data: { addedReply },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async deleteReplyHandler(request, h) {
+    const { id: owner } = request.auth.credentials;
+    const { threadId, commentId, replyId } = request.params;
+    const deleteReplyUseCase = this._container.getInstance(DeleteReplyUseCase.name);
+    await deleteReplyUseCase.execute(replyId, commentId, threadId, owner);
+
+    const response = h.response({
+      status: 'success',
+    });
+    return response;
+  }
+}
+
+module.exports = RepliesHandler;
