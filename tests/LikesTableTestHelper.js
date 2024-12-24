65627b55f2e1c4e30b7a67229aed69f827e0473a
Fauzan Rizky <frizky.erlang@gmail.com>
2024-12-24 23:35:34 +0700

    remove unneeded function

--- tests/LikesTableTestHelper.js
+++ tests/LikesTableTestHelper.js
@@ -26,16 +26,6 @@ const LikesTableTestHelper = {
     return result.rows;
   },
 
-  async findLikesByCommentIdAndUserId(commentId, userId) {
-    const query = {
-      text: 'SELECT * FROM likes WHERE comment_id = $1 AND user_id = $2',
-      values: [commentId, userId],
-    };
-
-    const result = await pool.query(query);
-    return result.rows;
-  },
-
   async cleanTable() {
     await pool.query('DELETE FROM likes WHERE 1=1');
   },
