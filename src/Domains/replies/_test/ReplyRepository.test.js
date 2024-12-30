e9d98469e37f34f10a922d07fba76d44336ad670
Fauzan Rizky <frizky.erlang@gmail.com>
2024-12-30 18:10:35 +0700

    add getRepliesByCommentIds

--- src/Domains/replies/_test/ReplyRepository.test.js
+++ src/Domains/replies/_test/ReplyRepository.test.js
@@ -18,6 +18,9 @@ describe('ReplyRepository interface', () => {
     await expect(replyRepository.getReplies('')).rejects.toThrowError(
       'REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED',
     );
+    await expect(replyRepository.getRepliesByCommentIds('')).rejects.toThrowError(
+      'REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED',
+    );
     await expect(replyRepository.deleteReply('')).rejects.toThrowError(
       'REPLY_REPOSITORY.METHOD_NOT_IMPLEMENTED',
     );
